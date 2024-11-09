9feb982100321d455c5f77918bffcd9a7f9900bf
Mostafa Omrane <momrane@paul-f3Br3s1.clusters.42paris.fr>
2024-11-09 14:59:51 +0100

    save

--- srcs/js/hello.js
+++ srcs/js/hello.js
@@ -1,2 +1,8 @@
 // app.js
 console.log("Hello, I am a Node.js app running in a Docker container!");
+
+// I want a loop that print Hi every 1 second
+setInterval(() => {
+	  console.log("Hi");
+}
+, 1000);
