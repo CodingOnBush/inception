7d5d7f90b182e6b1d3f6b7d83c3bf8e359610fbe
Mostafa Omrane <momrane@made-f0Ar2s6.clusters.42paris.fr>
2024-11-19 18:18:11 +0100

    save

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -3,11 +3,16 @@
 # Démarrer le service MySQL
 service mysql start
 
-# Créer la base de données et les utilisateurs
+# Créer la base de données et les utilisateurs #
+# Create database
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+# Create user
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+# Grant privileges
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
+# Modify root user
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# Refresh privileges
 mysql -e "FLUSH PRIVILEGES;"
 
 # Éteindre le service MySQL pour le relancer en mode sécurisé
