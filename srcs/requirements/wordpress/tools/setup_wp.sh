a0e32ffa1344bc48e3948950f1e5ed1747c31f95
Mostafa Omrane <momrane@paul-f4Ar1s4.clusters.42paris.fr>
2024-11-25 16:33:29 +0100

    save

--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -13,7 +13,7 @@
 
 # set -ex # print commands & exit on error (debug mode)
 
-# WP_URL=mos.42.fr
+# WP_URL=login.42.fr
 # WP_TITLE=Inception
 # WP_ADMIN_USER=theroot
 # WP_ADMIN_PASSWORD=123
@@ -26,7 +26,6 @@
 chown -R www-data:www-data /var/www/inception/
 
 if [ ! -f "/var/www/inception/wp-config.php" ]; then
-   echo "copying wp-config.php QPOWMDPQOMWDMQWMDPOMQWOPDQPOWMDPMQWDOMQPMWODPOQMWQPOWMDPQOMWDMQWMDPOMQWOPDQPOWMDPMQWDOMQPMWODPOQMWQPOWMDPQOMWDMQWMDPOMQWOPDQPOWMDPMQWDOMQPMWODPOQMW"
    mv /tmp/wp-config.php /var/www/inception/
 fi
 
@@ -37,20 +36,20 @@ wp --allow-root --path="/var/www/inception/" core download || true
 if ! wp --allow-root --path="/var/www/inception/" core is-installed;
 then
     wp  --allow-root --path="/var/www/inception/" core install \
-        --url=mos.42.fr \
-        --title=Inception \
-        --admin_user=theroot \
-        --admin_password=123 \
-        --admin_email=theroot@123.com
+        --url=$WP_URL \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL
 fi;
 
-if ! wp --allow-root --path="/var/www/inception/" user get theuser;
+if ! wp --allow-root --path="/var/www/inception/" user get $WP_USER;
 then
     wp  --allow-root --path="/var/www/inception/" user create \
-        theuser \
-        theuser@123.com \
-        --user_pass=abc \
-        --role=editor
+        $WP_USER \
+        $WP_EMAIL \
+        --user_pass=$WP_PASSWORD \
+        --role=$WP_ROLE
 fi;
 
 wp --allow-root --path="/var/www/inception/" theme install raft --activate 
