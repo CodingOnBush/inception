1a10a71c3095e9ccdecc08a7f1fe0eb8c9159c81
Mostafa Omrane <momrane@paul-f4Ar1s4.clusters.42paris.fr>
2024-11-25 14:33:27 +0100

    save

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,48 +1,96 @@
 <?php
-// ** Paramètres de la base de données ** //
-define('DB_NAME', getenv('MYSQL_DATABASE'));     // Nom de la base de données
-define('DB_USER', getenv('MYSQL_USER'));         // Nom d'utilisateur de la base
-define('DB_PASSWORD', getenv('MYSQL_PASSWORD')); // Mot de passe de la base
-define('DB_HOST', 'mariadb');                    // Adresse de l'hôte (nom du conteneur MariaDB)
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
 
-// Jeu de caractères à utiliser pour la base de données
-define('DB_CHARSET', 'utf8');
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
 
-// Type de collation de la base de données. Ne changez pas si vous n'êtes pas sûr.
-define('DB_COLLATE', '');
+define( 'DB_NAME', getenv('thedatabase') );
+define( 'DB_USER', getenv('theuser') );
+define( 'DB_PASSWORD', getenv('abc') );
+define( 'DB_HOST', getenv('mariadb') );
+define( 'WP_HOME', getenv('https://mos.42.fr') );
+define( 'WP_SITEURL', getenv('https://mos.42.fr') );
 
-// ** Clés et sels uniques d'authentification ** //
-define('AUTH_KEY',         'your_random_key_1');
-define('SECURE_AUTH_KEY',  'your_random_key_2');
-define('LOGGED_IN_KEY',    'your_random_key_3');
-define('NONCE_KEY',        'your_random_key_4');
-define('AUTH_SALT',        'your_random_key_5');
-define('SECURE_AUTH_SALT', 'your_random_key_6');
-define('LOGGED_IN_SALT',   'your_random_key_7');
-define('NONCE_SALT',       'your_random_key_8');
+// define( 'DB_NAME', getenv('DB_NAME') );
+// define( 'DB_USER', getenv('DB_USER') );
+// define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
+// define( 'DB_HOST', getenv('DB_HOST') );
+// define( 'WP_HOME', getenv('WP_FULL_URL') );
+// define( 'WP_SITEURL', getenv('WP_FULL_URL') );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
 
-// Vous pouvez générer ces clés automatiquement sur https://api.wordpress.org/secret-key/1.1/salt/
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '' );
+define( 'SECURE_AUTH_KEY',  '' );
+define( 'LOGGED_IN_KEY',    '' );
+define( 'NONCE_KEY',        '' );
+define( 'AUTH_SALT',        '' );
+define( 'SECURE_AUTH_SALT', '' );
+define( 'LOGGED_IN_SALT',   '' );
+define( 'NONCE_SALT',       '' );
 
-// Préfixe des tables de la base de données
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
 $table_prefix = 'wp_';
 
-// Activer/désactiver le mode débogage
-define('WP_DEBUG', false);
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', true );
 
-// ** Réglages avancés ** //
-// Définir l'URL du site
-define('WP_HOME', 'https://yourdomain.com');     // Remplacez par votre domaine
-define('WP_SITEURL', 'https://yourdomain.com');
+/* Add any custom values between this line and the "stop editing" line. */
 
-// Répertoires pour les fichiers WordPress
-define('WP_CONTENT_DIR', '/var/www/html/wp-content');
 
-// Définit si les fichiers doivent être corrigés dans un environnement en lecture seule
-define('FS_METHOD', 'direct');
 
-// Chemin absolu vers le dossier WordPress
-if ( !defined('ABSPATH') )
-    define('ABSPATH', dirname(__FILE__) . '/');
+/* That's all, stop editing! Happy publishing. */
 
-// Inclure les fichiers WordPress
-require_once(ABSPATH . 'wp-settings.php');
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
